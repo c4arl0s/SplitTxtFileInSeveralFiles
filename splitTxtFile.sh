e656bb501f6a2e178e627421dab7f6402eb8a4dd
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-12 20:57:43 -0600

    add echo command to remove white spaces in a string

--- splitTxtFile.sh
+++ splitTxtFile.sh
@@ -5,8 +5,11 @@ echo -e "number of lines $numberOfLines"
 
 cat glossary.txt | while read line; do
 	
-	word=`echo $line | cut -f 1 -d ":" |  tr [A-Z] [a-z]`
+	word=`echo $line | cut -f 1 -d ":" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
 	echo "$word"
+	renameNoSpace=`echo ${word/ /}`
+	touch $renameNoSpace.txt
+	echo "$line" > $renameNoSpace.txt	
 
 done
 
