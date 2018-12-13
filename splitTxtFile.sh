683f2dade2c423640dcdf28ba5a152799bcf7a7c
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-12 21:23:55 -0600

    Remove white spaces with tr command from definition variable

--- splitTxtFile.sh
+++ splitTxtFile.sh
@@ -5,11 +5,13 @@ echo -e "number of lines $numberOfLines"
 
 cat glossary.txt | while read line; do
 	
-	word=`echo $line | cut -f 1 -d ":" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
-	echo "$word"
-	renameNoSpace=`echo ${word/ /}`
-	touch $renameNoSpace.txt
-	echo "$line" > $renameNoSpace.txt	
+	definition=`echo $line | cut -f 1 -d ":" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
+	echo "$definition"
+	#renameNoSpace=`sed '/^ *$/d' $definition`
+	definitionNoSpace=`echo "$definition" | tr -d ' '`
+	#touch $renameNoSpace.txt
+	echo "$definitionNoSpace"
+	#echo "$line" > $renameNoSpace.txt	
 
 done
 
