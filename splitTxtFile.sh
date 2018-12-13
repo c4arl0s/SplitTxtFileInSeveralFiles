8c46942a3ada590fdcb843870673a6344e732210
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-12 20:41:00 -0600

    Add tr command

--- splitTxtFile.sh
+++ splitTxtFile.sh
@@ -2,4 +2,11 @@
 
 numberOfLines=`wc -l glossary.txt | cut -f 6 -d " "`
 echo -e "number of lines $numberOfLines"
-cat glossary.txt | head -100 | tail -1 | cut -f 2 -d ":"
+
+cat glossary.txt | while read line; do
+	
+	word=`echo $line | cut -f 1 -d ":" |  tr [A-Z] [a-z]`
+	echo "$word"
+
+done
+
