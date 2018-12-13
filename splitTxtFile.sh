b97436266c456f058a00750d29f9a5610d84a967
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-12 21:26:43 -0600

    Execute script, rename all files

--- splitTxtFile.sh
+++ splitTxtFile.sh
@@ -7,11 +7,10 @@ cat glossary.txt | while read line; do
 	
 	definition=`echo $line | cut -f 1 -d ":" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
 	echo "$definition"
-	#renameNoSpace=`sed '/^ *$/d' $definition`
 	definitionNoSpace=`echo "$definition" | tr -d ' '`
-	#touch $renameNoSpace.txt
+	touch $definitionNoSpace.txt
 	echo "$definitionNoSpace"
-	#echo "$line" > $renameNoSpace.txt	
+	echo "$line" > $definitionNoSpace.txt	
 
 done
 
