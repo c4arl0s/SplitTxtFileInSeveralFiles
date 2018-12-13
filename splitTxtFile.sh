0ade1594c49a89656c1ff2cf04f41ca9da1d3223
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-13 13:53:45 -0600

    print variable with capitalized first leter

--- splitTxtFile.sh
+++ splitTxtFile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+file=$1
 echo "pattern in the file -> definition:Meaning"
-numberOfLines=`wc -l glossary.txt | cut -f 6 -d " "`
+numberOfLines=`wc -l $file.txt | cut -f 6 -d " "`
 echo -e "number of lines $numberOfLines"
 
 cat glossary.txt | while read line
@@ -8,6 +9,7 @@ do
 	definition=`echo $line | cut -f 1 -d ":"`
 	echo "$definition"
 	definitionFirstLetterCapitalized=`echo $definition | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
+	echo "$definitionFirstLetterCapitalized"
 	definitionRemoveWhiteSpaces=`echo "$definitionFirstLetterCapitalized" | tr -d ' '`
 	touch $definitionRemoveWhiteSpaces.txt
 	echo "$definitionRemoveWhiteSpaces"
